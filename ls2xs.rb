461ee4725fcca63e35668799d94f102b7bd06db0
Masayuki Ataka <masayuki.ataka@gmail.com>
2015-09-17 20:40:50 +0900

    Update ls2xs.rb

--- ls2xs.rb
+++ ls2xs.rb
@@ -1,5 +1,7 @@
+require "formula"
+
 class Ls2xs < Formula
-  homepage "https://github.com/ishkawa/LocalizableStrings2XibStrings"
+  homepage "https://github.com/ataka/LocalizableStrings2XibStrings"
   url "https://github.com/ataka/ls2xs/archive/0.0.2.tar.gz"
   version "0.0.2"
   sha256 "da59320a1fb8b80faa677e69e763488ea7c9eeb4ef5b30a2febd5454ba672a30"

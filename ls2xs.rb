80422093c351a463f6c45edbcb90f02b1ea4aea9
ataka <masayuki.ataka@gmail.com>
2015-09-17 19:08:17 +0900

    Update ls2xs
    Copied from https://github.com/ishkawa/homebrew-formulae/blob/master/ls2xs.rb

--- ls2xs.rb
+++ ls2xs.rb
@@ -1,39 +1,10 @@
-# Documentation: https://github.com/Homebrew/homebrew/blob/master/share/doc/homebrew/Formula-Cookbook.md
-#                /usr/local/Library/Contributions/example-formula.rb
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
-
 class Ls2xs < Formula
-  desc ""
-  homepage ""
+  homepage "https://github.com/ishkawa/LocalizableStrings2XibStrings"
   url "https://github.com/ataka/ls2xs/archive/0.0.2.tar.gz"
   version "0.0.2"
   sha256 "da59320a1fb8b80faa677e69e763488ea7c9eeb4ef5b30a2febd5454ba672a30"
 
-  # depends_on "cmake" => :build
-  depends_on :x11 # if your formula requires any X11/XQuartz components
-
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
-    system "make", "install" # if this fails, try separate make/make install steps
-  end
-
-  test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! It's enough to just replace
-    # "false" with the main program this formula installs, but it'd be nice if you
-    # were more thorough. Run the test with `brew test ls2xs`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    system "make prefix_install PREFIX=#{prefix}"
   end
 end

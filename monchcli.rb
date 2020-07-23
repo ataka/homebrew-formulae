14dea69c6d1181d64e2ac967e2ae477c84c2a1cc
Masayuki Ataka <masayuki.ataka@gmail.com>
2020-07-24 07:55:13 +0900

    Rename MonchCLI.rb to monchcli.rb

--- monchcli.rb
+++ monchcli.rb
@@ -1,12 +1,14 @@
 require "formula"
 
-class MonchCLI < Formula
+class Monchcli < Formula
+  desc "A command line tool that "
   homepage "https://github.com/ataka/MonchCLI"
-  url "https://github.com/ataka/MonchCLI/archive/0.5.0.tar.gz"
+  url "https://github.com/ataka/MonchCLI"
   version "0.5.0"
   sha256 "632bf87feaa5671757e9aa1bf7181749218fb7368a09a73cb47a9f851422d742"
+  license "MIT"
 
   def install
-    system "make prefix_install PREFIX=#{prefix}"
+    system "make", "prefix_install", "PREFIX=#{prefix}"
   end
 end

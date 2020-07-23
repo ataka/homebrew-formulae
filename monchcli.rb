71a4a6a3df1cb8775035b2a3e365e71b6eb53aae
Masayuki Ataka <masayuki.ataka@gmail.com>
2020-07-24 07:59:33 +0900

    monchcli: Fix url

--- monchcli.rb
+++ monchcli.rb
@@ -3,7 +3,7 @@ require "formula"
 class Monchcli < Formula
   desc "A command line tool that "
   homepage "https://github.com/ataka/MonchCLI"
-  url "https://github.com/ataka/MonchCLI"
+  url "https://github.com/ataka/MonchCLI/archive/0.5.0.tar.gz"
   version "0.5.0"
   sha256 "632bf87feaa5671757e9aa1bf7181749218fb7368a09a73cb47a9f851422d742"
   license "MIT"

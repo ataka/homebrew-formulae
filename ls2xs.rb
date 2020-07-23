9f2a9910ac5ffe7ab2ae5347c84f321a70129ea0
Masayuki Ataka <masayuki.ataka@gmail.com>
2020-07-23 09:43:36 +0900

    Update ls2xs version 0.3.0

--- ls2xs.rb
+++ ls2xs.rb
@@ -1,10 +1,10 @@
 require "formula"
 
 class Ls2xs < Formula
-  homepage "https://github.com/ataka/LocalizableStrings2XibStrings"
-  url "https://github.com/ataka/ls2xs/archive/0.0.2.tar.gz"
-  version "0.0.2"
-  sha256 "a644361b451dabac966ce6e87739d7015900f825a29f0141c29e07cea7dc02c0"
+  homepage "https://github.com/ataka/ls2xs"
+  url "https://github.com/ataka/ls2xs/archive/0.3.0.tar.gz"
+  version "0.3.0"
+  sha256 "b20e5d1ab6f8516de40ce59a85e62ba12d5ec0aa8ed2349301b8e36aa488210a"
 
   def install
     system "make prefix_install PREFIX=#{prefix}"

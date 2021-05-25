bdf17c78538989b6a5503806b9b462e61c4df2c4
Masayuki Ataka <masayuki.ataka@gmail.com>
2021-05-26 00:35:04 +0900

    Update MonchCLI version 0.6.0

--- monchcli.rb
+++ monchcli.rb
@@ -3,9 +3,9 @@ require "formula"
 class Monchcli < Formula
   desc "A command line tool to simplify the review request"
   homepage "https://github.com/ataka/MonchCLI"
-  url "https://github.com/ataka/MonchCLI/archive/0.5.0.tar.gz"
-  version "0.5.0"
-  sha256 "632bf87feaa5671757e9aa1bf7181749218fb7368a09a73cb47a9f851422d742"
+  url "https://github.com/ataka/MonchCLI/archive/0.6.0.tar.gz"
+  version "0.6.0"
+  sha256 "6c4a79b214d0fd7c810f3400703723c4302f7ab778a0652272fe0c4d3494f871"
   license "MIT"
 
   def install

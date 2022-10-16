398deec5fae876c45cd15d5fa5b13f6bca8d4e01
Masayuki Ataka <masayuki.ataka@gmail.com>
2022-10-17 00:50:41 +0900

    Update ls2xs version 0.4.0

--- ls2xs.rb
+++ ls2xs.rb
@@ -3,9 +3,9 @@ require "formula"
 class Ls2xs < Formula
   desc "A command line tool to update .strings files of *.xib and *.storyboard using Localizable.strings"
   homepage "https://github.com/ataka/ls2xs"
-  url "https://github.com/ataka/ls2xs/archive/0.3.1.tar.gz"
-  version "0.3.1"
-  sha256 "273bcdebd8579e89f34f0f354fd28fd6d0a0a8d5c1e633f450f8813f25443664"
+  url "https://github.com/ataka/ls2xs/archive/0.4.0.tar.gz"
+  version "0.4.0"
+  sha256 "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855"
   license "MIT"
 
   def install
